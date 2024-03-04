5388db31ef2a39caaa2eefc97da860b20702f152
Rehab Sabry <rehab.sabry.000@gmail.com>
2024-03-05 01:31:57 +0200

    react lab3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Products from './Components/LCH/Products';
+import Layout from './Components/Layout/Layout';
+import AddProduct from './Components/LCH/AddProduct';
+import NotFound from './Components/NotFound/NotFound';
 
+let routers = createBrowserRouter([
+  {path:'/' ,   element: <Layout showNavbar={true}/> , children:[
+    {index: true, element: <Products/>},
+    {path:'add' , element: <AddProduct/>},
+    {path:'*' , element:<NotFound/>}
+
+  ]},
+]);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return <RouterProvider router={routers}/>;
+    
 }
 
 export default App;
